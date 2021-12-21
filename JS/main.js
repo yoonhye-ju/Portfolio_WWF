2e948b0795bb2cbc9e335f1eb92f4e75f18c1a36
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2021-12-21 17:43:35 +0900

    revise
    revise

--- JS/main.js
+++ JS/main.js
@@ -177,20 +177,20 @@ function slide() {
     $('.slide').css({ marginLeft: 0 });
   });
 }
-setInterval(slide, 3000);
+setInterval(slide, 5000);
 
 
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
   showSlides(slideIndex += n);}
 function currentSlide(n) {
   showSlides(slideIndex = n);}
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("historyBox");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("historyBox");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
