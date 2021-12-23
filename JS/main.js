5e76e49c30a2298685a8bfc73646dbdbeba4b790
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2021-12-23 18:01:04 +0900

    revise
    revise

--- JS/main.js
+++ JS/main.js
@@ -138,35 +138,31 @@ $(document).ready(function(){
   $('.movie').mouseover(function(){
     $(this).children('.icon_video').show();
   });
-});
 
-$(function(){
-  $('.icon_video1').click(function(){
-    $('.popup1').css('display','block')();
-  });
-  $('#close').click(function(){
-    $('.popup1').css('display','none')();
+
+  $('.movie').eq(0).children('.icon_video').click(function(){
+    $('#pop').css('display','block');
+    $('.video1').css('display','block');
   });
 
-  $('.icon_video2').click(function(){
-    $('.popup2').css('display','block')();
-  });
-  $('#close').click(function(){
-    $('.popup2').css('display','none')();
+  $('.movie').eq(1).children('.icon_video').click(function () {
+    $('#pop').css('display','block');
+    $('.video2').css('display', 'block');
   });
 
-  $('.icon_video3').click(function(){
-    $('.popup3').css('display','block')();
-  });
-  $('#close').click(function(){
-    $('.popup3').css('display','none')();
+  $('.movie').eq(2).children('.icon_video').click(function () {
+    $('#pop').css('display','block');
+    $('.video3').css('display', 'block');
   });
 
-  $('.icon_video4').click(function(){
-    $('.popup4').css('display','block')();
+  $('.movie').eq(3).children('.icon_video').click(function () {
+    $('#pop').css('display','block');
+    $('.video4').css('display', 'block');
   });
-  $('#close').click(function(){
-    $('.popup4').css('display','none')();
+
+  $('.close').click(function(){
+    $('#pop').css('display','none');
+    $('.popup').css('display','none');
   });
 });
 
