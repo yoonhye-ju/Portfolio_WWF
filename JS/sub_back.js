20539768ca4a3a69ae4c1a8df118e0b9a2bb5b76
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2021-12-30 18:51:47 +0900

    revise
    revise

--- JS/sub_back.js
+++ JS/sub_back.js
@@ -1,13 +1,13 @@
 
-$(document).ready(function(){
-$('.emails').change(function(){
-  let val=$('.emails option:selected').val();
-  if(val == 'email1'){
+$(document).ready(function () {
+  $('.emails').change(function () {
+    let val = $('.emails option:selected').val();
+    if (val == 'email1') {
       $("input[type=email]").attr('placeholder', 'ex) wwf@wwf.co.kr');
-  } else{
+    } else {
       $("input[type=email]").attr('placeholder', '');
-  }
-});
+    }
+  });
 });
 
 $(document).ready(function () {
@@ -26,3 +26,17 @@ $(document).ready(function () {
     alert("옳지 않은 계좌 정보 입니다.")
   });
 });
+
+$(document).ready(function () {
+  $('.donateButton').click(function () {
+    alert("후원 정보를 입력해주세요.")
+  });
+});
+
+
+function selectAll(selectAll) {
+  let checkboxes = document.querySelectorAll("input[type='checkbox']");
+  checkboxes.forEach(function (checkbox) {
+    checkbox.checked = selectAll.checked
+  })
+}
