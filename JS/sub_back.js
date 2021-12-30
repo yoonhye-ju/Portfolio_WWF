e5e9de7ecb26ae7ec00a04b20def833c90a40e48
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2021-12-30 17:35:43 +0900

    revise
    revise

--- JS/sub_back.js
+++ JS/sub_back.js
@@ -1,10 +1,28 @@
-window.onload = function () {
 
-  var arrColor = ["img/sub_background.jpg", "img/sub_backgroundB.jpg", "img/sub_backgroundC.jpg"];
-  function chc() {
-    var arrNum = Math.floor(Math.random() * arrColor.length);
-    back.style.backgroundImage = url(arrColor[arrNum]);
+$(document).ready(function(){
+$('.emails').change(function(){
+  let val=$('.emails option:selected').val();
+  if(val == 'email1'){
+      $("input[type=email]").attr('placeholder', 'ex) wwf@wwf.co.kr');
+  } else{
+      $("input[type=email]").attr('placeholder', '');
   }
+});
+});
 
-  setInterval(chc, 1000);
-}
+$(document).ready(function () {
+  $('.banks').change(function () {
+    let state = $('.banks option:selected').val();
+    if (state == 'bank1') {
+      $('.selfBank').show();
+    } else {
+      $('.selfBank').hide();
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('.check').click(function () {
+    alert("옳지 않은 계좌 정보 입니다.")
+  });
+});
