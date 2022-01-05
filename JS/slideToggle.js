e19378cd0edaf50de1a149d196b1c5baa3289ed0
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-01-05 17:36:55 +0900

    revise
    revise

--- JS/slideToggle.js
+++ JS/slideToggle.js
@@ -2,6 +2,12 @@ $(function(){
   $('.menuButton').click(function(){
     $('.menu').slideToggle();
     $('.donateRight').toggleClass("rightTop");
-    $('#back').css({height: "2800px"});
+    $('#back').toggleClass("backTop");
   });
+});
+
+$(window).resize(function(){
+  if(window.innerWidth>=500){
+      $(".menu").show();
+  }
 });
